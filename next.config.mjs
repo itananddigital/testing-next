73a26afbd8353c1282c2bbb4ce1876d0a2666e3f
Anand Mohan <anand1505mohan@gmail.com>
2024-08-05 13:42:46 +0530

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/testing-next",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
   };
